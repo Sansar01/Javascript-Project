35b6b8b1a7a02dc9dfe89002cd14688a8fbf00ff
Sansar Tiwari <89459552+Sansar01@users.noreply.github.com>
2023-09-26 11:01:45 +0530

    Update script.js

--- GuessTheNumber/script.js
+++ GuessTheNumber/script.js
@@ -1,7 +1,7 @@
 let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector("#subt");
-const userInput = document.querySelector('#guessField');
+const userInput = document.querySelector('#guessfield');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
